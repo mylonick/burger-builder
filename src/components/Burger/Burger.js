86a38b0b9d4da9c91a399416dfaa8a44d21505b5
mylonick <mylonick@hotmail.com>
2020-06-07 21:19:03 +0300

    Connecting the state with build controls and implemented the add ingredient functionality

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,9 +3,11 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+
     //Extracts the keys of an object and returns that into an array
     //Then with map, execute a function on each element in the input array
-    //Class 130 !
+    //TODO CLASS 130
     let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) =>{
              return <BurgerIngredient key={igKey+i} type={igKey} />
