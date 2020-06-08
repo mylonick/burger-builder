0842deac8f677e94310216b35899507dc02fefd7
mylonick <mylonick@hotmail.com>
2020-06-08 19:29:17 +0300

    Improved Folder Structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 //import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from "../../UI/Button/Button";
 
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-import classes from '../Layout/Layout.css';
-import sideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import classes from './Layout.css';
+import sideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 
 class Layout extends Component {
