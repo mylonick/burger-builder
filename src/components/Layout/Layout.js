da94c0c1124f56d1e1423304cf599d9776491714
mylonick <mylonick@hotmail.com>
2020-06-08 16:08:05 +0300

    Added SideDrawer Toggle Button & Functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from '../Layout/Layout.css';
+import sideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
+    state = {
+        showSideDrawer : true
+    }
 
-);
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer : false})
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+
+        //NOT LIKE THIS
+        // this.setState({showSideDrawer : !this.state.showSideDrawer})
+        //LIKE THIS
+        this.setState((prevState) => {
+            return {showSideDrawer : !prevState.showSideDrawer};
+        });
+
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,25 +2,34 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
 
 
-    return(
+    let attachedClasses = [classes.SideDrawer, classes.Close];
 
-        <div className={classes.SideDrawer}>
-            <Backdrop/>
-            <div className={classes.Logo}>
-                <Logo/>
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer,  classes.Open];
+    }
+
+    return(
+        <Aux>
+                <Backdrop show={props.open} clicked={props.closed} />
+                    <div className={attachedClasses.join(' ')}>
+                    <div className={classes.Logo}>
+                        <Logo/>
+                    </div>
+
+                    <nav>
+                        <NavigationItems/>
+                    </nav>
+
             </div>
 
-            <nav>
-                <NavigationItems/>
-            </nav>
-
-        </div>
+        </Aux>
 
     );
 }
