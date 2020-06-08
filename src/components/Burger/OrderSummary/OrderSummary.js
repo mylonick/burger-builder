ee65b29bfd10e08a6d44459d5d9e6fbf2540f16a
mylonick <mylonick@hotmail.com>
2020-06-08 19:21:33 +0300

    Improved Performance with Lifecycle Hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            // Outer {} : dynamic entry , Inner {} : Javascript Object
-            return (
-                //Because a warning is shown i add a key
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}> {igKey} </span> : {props.ingredients[igKey]}
-                </li>);
-        });
+class OrderSummary extends Component {
+    //This could be a functional Components. Doesn't have to be a Class
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients : </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
-            <p>Continue to Checkout ?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
 
-export default orderSummary;
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                // Outer {} : dynamic entry , Inner {} : Javascript Object
+                return (
+                    //Because a warning is shown i add a key
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}> {igKey} </span> : {this.props.ingredients[igKey]}
+                    </li>);
+            });
+
+        return(
+
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients : </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong> </p>
+                <p>Continue to Checkout ?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+
+        )
+    }
+
+
+}
+
+
+export default OrderSummary;
